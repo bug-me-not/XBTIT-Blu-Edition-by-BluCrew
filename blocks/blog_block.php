7ff0c8f110ee16eabf4f05e81dc79633d2291558
HDVinnie <Vinnie981@gmail.com>
2016-05-10 16:54:41 -0400

    Blog Block

---
+++ blocks/blog_block.php
@@ -0,0 +1,10 @@
+<div class="panel panel-primary">
+<div class="panel-heading">
+<h4 class="text-center">News</h4>
+</div>
+<div class="jumbotron">
+   <h1>Dev Blog</h1>
+   <p>Be sure to check out our dev blog to view latest site announments and news.</p>
+   <p><a class="btn btn-primary btn-md">Read More</a></p>
+</div>
+</div>
