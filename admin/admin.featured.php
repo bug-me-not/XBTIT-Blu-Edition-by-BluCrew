198896d8c8337c77bc65a686f548a92f89c6a256
MrG01 <MrG01@users.noreply.github.com>
2016-05-12 15:53:25 +0200

    SQL fix

--- admin/admin.featured.php
+++ admin/admin.featured.php
@@ -34,10 +34,10 @@
 ////////////////////////////////////////////////////////////////////////////////////
 
 if (!defined("IN_BTIT"))
-      die("non direct access!");
+  die("non direct access!");
 
 if (!defined("IN_ACP"))
-      die("non direct access!");
+  die("non direct access!");
 
 global $THIS_BASEPATH, $language, $THE_BASEPATH;
 
@@ -50,70 +50,69 @@ $admintpl->set("frm_action","index.php?page=admin&amp;user=".$CURUSER["uid"]."&a
 include(load_language("lang_featured.php"));
 
 switch ($action)
-    {
-case 'save':
-        if ($_POST["id"])
-            {
-             $tor_id = $_POST["id"];
-             do_sqlquery("INSERT INTO {$TABLE_PREFIX}featured (fid,torrent_id) VALUES ('','$_POST[id]')");
-	        }
+{
+  case 'save':
+  if ($_POST["id"])
+  {
+   $tor_id = $_POST["id"];
+   do_sqlquery("INSERT INTO {$TABLE_PREFIX}featured (fid,torrent_id) VALUES ('','$tor_id')");
+ }
 
-        case 'read':
-        default: 
-if ($btit_settings["imdbbl"]==true ) 
-$query = mysql_query("SELECT info_hash, filename from {$TABLE_PREFIX}files where imdb!='' ORDER BY data DESC limit 15");
+ case 'read':
+ default: 
+ if ($btit_settings["imdbbl"]==true ) 
+  $query = do_sqlquery("SELECT info_hash, filename from {$TABLE_PREFIX}files where imdb!='' ORDER BY data DESC limit 15");
 else       
-$query = mysql_query("SELECT info_hash, filename from {$TABLE_PREFIX}files where image!='' ORDER BY data DESC limit 15");
-			
+  $query = do_sqlquery("SELECT info_hash, filename from {$TABLE_PREFIX}files where image!='' ORDER BY data DESC limit 15");
+
 
 $torrents=array();
 $i=0;
-while ($results=mysql_fetch_array($query)) {
+while ($results=$query->fetch_array()) {
 
-$torrents[$i]["hash"]= "<tr><td><input type=radio value=".$results['info_hash']." name=id>";
-$torrents[$i]["name"]= " - ".$results['filename']."</td></tr>";
+  $torrents[$i]["hash"]= "<tr><td><input type=radio value=".$results['info_hash']." name=id>";
+  $torrents[$i]["name"]= " - ".$results['filename']."</td></tr>";
 
-$i++;
+  $i++;
 }
-		unset($result);
-          mysql_free_result($query);
+unset($result);
+$query->free();
 
 $admintpl->set("TORRENT",$torrents);
 
 // This is the part that will print the currently featured torrent
 if ($XBTT_USE)
-   {
-    $tseeds="f.seeds+ifnull(x.seeders,0) as seeds";
-    $tleechs="f.leechers+ifnull(x.leechers,0) as leechers";
-    $tcompletes="f.finished+ifnull(x.completed,0) as finished";
-    $ttables="{$TABLE_PREFIX}files f LEFT JOIN xbt_files x ON x.info_hash=f.bin_hash";
-   }
+{
+  $tseeds="f.seeds+ifnull(x.seeders,0) as seeds";
+  $tleechs="f.leechers+ifnull(x.leechers,0) as leechers";
+  $tcompletes="f.finished+ifnull(x.completed,0) as finished";
+  $ttables="{$TABLE_PREFIX}files f LEFT JOIN xbt_files x ON x.info_hash=f.bin_hash";
+}
 else
-    {
-    $tseeds="f.seeds as seeds";
-    $tleechs="f.leechers as leechers";
-    $tcompletes="f.finished as finished";
-    $ttables="{$TABLE_PREFIX}files f";
-    }
+{
+  $tseeds="f.seeds as seeds";
+  $tleechs="f.leechers as leechers";
+  $tcompletes="f.finished as finished";
+  $ttables="{$TABLE_PREFIX}files f";
+}
 
-$sql = mysql_query("SELECT * FROM {$TABLE_PREFIX}featured ORDER BY fid DESC limit 1");
-    $result = mysql_fetch_assoc($sql);
+$sql = do_sqlquery("SELECT * FROM {$TABLE_PREFIX}featured ORDER BY fid DESC limit 1");
+$result = $sql->fetch_assoc();
 
-$torrent = mysql_query("SELECT f.info_hash, f.filename, f.url, UNIX_TIMESTAMP(f.data) as data, f.size, f.comment, f.uploader, c.name as cat_name, c.image as cat_image, c.id as cat_id, $tseeds, $tleechs, $tcompletes, f.speed, f.external, f.announce_url,UNIX_TIMESTAMP(f.lastupdate) as lastupdate,UNIX_TIMESTAMP(f.lastsuccess) as lastsuccess, f.anonymous, u.username FROM $ttables LEFT JOIN {$TABLE_PREFIX}categories c ON c.id=f.category LEFT JOIN {$TABLE_PREFIX}users u ON u.id=f.uploader WHERE f.info_hash ='$result[torrent_id]'");
+$torrent = do_sqlquery("SELECT f.info_hash, f.filename, f.url, UNIX_TIMESTAMP(f.data) as data, f.size, f.comment, f.uploader, c.name as cat_name, c.image as cat_image, c.id as cat_id, $tseeds, $tleechs, $tcompletes, f.speed, f.external, f.announce_url,UNIX_TIMESTAMP(f.lastupdate) as lastupdate,UNIX_TIMESTAMP(f.lastsuccess) as lastsuccess, f.anonymous, u.username FROM $ttables LEFT JOIN {$TABLE_PREFIX}categories c ON c.id=f.category LEFT JOIN {$TABLE_PREFIX}users u ON u.id=f.uploader WHERE f.info_hash =\"".$result['torrent_id']."\"");
 
 $featured=array();
 $a=0;
 
-while ($tor = mysql_fetch_assoc($torrent)) {
-$description = format_comment($tor["comment"]);
-$featured[$a]["name"]="<a href=/index.php?page=torrent-details&id=".$tor['info_hash'].">".$tor[filename]."</a>";
-$featured[$a]["cat"]="<a href=\"index.php?page=torrents&amp;category=$tor[cat_id]\">" . image_or_link( ($tor["cat_image"] == "" ? "" : "$STYLEPATH/images/categories/large/" . $tor["cat_image"]), "", $tor["cat_name"]) . "</a>";
-$featured[$a]["sl"]="<br /><b>Seeders: </b>".$tor[seeds]."<br />
-    <b>Leechers: </b>".$tor[leechers]."";
+while ($tor = $torrent->fetch_assoc() )
+{
+  $description = format_comment($tor["comment"]);
+  $featured[$a]["name"]="<a href=/index.php?page=torrent-details&id=".$tor['info_hash'].">".$tor['filename']."</a>";
+  $featured[$a]["cat"]="<a href=\"index.php?page=torrents&amp;category=".$tor['cat_id']."\">" . image_or_link( ($tor["cat_image"] == "" ? "" : "$STYLEPATH/images/categories/large/" . $tor["cat_image"]), "", $tor["cat_name"]) . "</a>";
+  $featured[$a]["sl"]="<br /><b>Seeders: </b>".$tor['seeds']."<br />
+  <b>Leechers: </b>".$tor['leechers']."";
 //description
-	$featured[$a]["desc"]="<table border=0 style=\"border:0px\" width=\"99%\"><tr><td>   
-".$description."
-  </td></tr></table>";
+  $featured[$a]["desc"]="<table border=0 style=\"border:0px\" width=\"99%\"><tr><td>".((strlen($description)>40)?substr($description,0,40):$description)."</td></tr></table>";
 
 $a++;
 }
