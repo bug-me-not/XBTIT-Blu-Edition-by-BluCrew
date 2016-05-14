3314bcb3a7bbae74ed70c3ee2aceb365c0bc6d45
MrG01 <MrG01@users.noreply.github.com>
2016-05-14 13:28:06 +0200

    User CP update for KOCS

--- user/usercp.menu.php
+++ user/usercp.menu.php
@@ -31,44 +31,46 @@
 ////////////////////////////////////////////////////////////////////////////////////
 
 if (!defined("IN_BTIT"))
-      die("non direct access!");
-
-
-
+  die("non direct access!");
 
 $usercp_menu=array(
-0=>array(
+    0=>array(
         "title"=>$language["MNU_UCP_HOME"],
         "menu"=>array(0=>array(
-                "url"=>"index.php?page=usercp&amp;uid=".$uid."" ,
-                "description"=>$language["MNU_UCP_HOME"]))
+            "url"=>"index.php?page=usercp&amp;uid=".$uid."" ,
+            "description"=>$language["MNU_UCP_HOME"]),
+        1=>array(
+            "title"=>$language["UCP_KHEZ"],
+            'menu'=>array(
+            # ==Khez==
+                )))
         ),
-1=>array(
+    1=>array(
         "title"=>$language["MNU_UCP_PM"],
         "menu"=>array(0=>array(
-                "url"=>"index.php?page=usercp&amp;uid=".$uid."&amp;do=pm&amp;action=list&amp;what=inbox" ,
-                "description"=>$language["MNU_UCP_PM"]),
-                      1=>array(
-                "url"=>"index.php?page=usercp&amp;uid=".$uid."&amp;do=pm&amp;action=list&amp;what=outbox" ,
-                "description"=>$language["MNU_UCP_OUT"]),
+            "url"=>"index.php?page=usercp&amp;uid=".$uid."&amp;do=pm&amp;action=list&amp;what=inbox" ,
+            "description"=>$language["MNU_UCP_PM"]),
+        1=>array(
+            "url"=>"index.php?page=usercp&amp;uid=".$uid."&amp;do=pm&amp;action=list&amp;what=outbox" ,
+            "description"=>$language["MNU_UCP_OUT"]),
 
-                      2=>array(
-                "url"=>"index.php?page=usercp&amp;do=pm&amp;action=edit&amp;uid=".$uid."&amp;what=new" ,
-                "description"=>$language["MNU_UCP_NEWPM"]), 
-                             )),
-2=>array(
+        2=>array(
+            "url"=>"index.php?page=usercp&amp;do=pm&amp;action=edit&amp;uid=".$uid."&amp;what=new" ,
+            "description"=>$language["MNU_UCP_NEWPM"]), 
+        )),
+    2=>array(
         "title"=>$language["MNU_UCP_INFO"],
         "menu"=>array(0=>array(
-                "url"=>"index.php?page=usercp&amp;do=user&amp;action=change&amp;uid=".$uid."" ,
-                "description"=>$language["MNU_UCP_INFO"]),
-                      1=>array(
-                "url"=>"index.php?page=usercp&amp;do=pwd&amp;action=change&amp;uid=".$uid."" ,
-                "description"=>$language["MNU_UCP_CHANGEPWD"]),
-                      2=>array(
-                "url"=>"index.php?page=usercp&amp;do=pid_c&amp;action=change&amp;uid=".$uid."" ,
-                "description"=>$language["CHANGE_PID"]), 
-                             )),
-);
+            "url"=>"index.php?page=usercp&amp;do=user&amp;action=change&amp;uid=".$uid."" ,
+            "description"=>$language["MNU_UCP_INFO"]),
+        1=>array(
+            "url"=>"index.php?page=usercp&amp;do=pwd&amp;action=change&amp;uid=".$uid."" ,
+            "description"=>$language["MNU_UCP_CHANGEPWD"]),
+        2=>array(
+            "url"=>"index.php?page=usercp&amp;do=pid_c&amp;action=change&amp;uid=".$uid."" ,
+            "description"=>$language["CHANGE_PID"]), 
+        )),
+    );
 
 if($btit_settings["fmhack_avatar_signature_sync"]=="enabled"){
 	$usercp_menu[3]["title"]=$language["SIG_EX"];
