3d3916e4a852f35f3b2792fbd3b81711c4713bfb
HDVinnie <Vinnie981@gmail.com>
2016-06-11 00:42:44 -0400

    Donors perks modal

--- blocks/paypal_block.php
+++ blocks/paypal_block.php
@@ -25,7 +25,79 @@ print("<p align=\"center\" class='text-danger'>(Visa/Mastercard/Discover)</p><br
 print "<a target=\"_blank\" style=\"border:none;\" href=\"http://www.gofundme.com/kfukmgyc?utm_medium=wdgt\" title=\"Visit this page now.\"class=\"donate\">DONATE NOW</a><br />";
 //go fund me
 
-print "<br><a href=index.php?page=forum&action=viewtopic&topicid=1600&pages=1>Donor's Perks Here</a>&nbsp;";
+print '<br><button class="btn btn-danger btn-xs" type="button" data-toggle="modal" data-target=".bs-example-modal-lg">Donors Perks</button>
+<div class="modal fade bs-example-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
+<div class="modal-dialog modal-lg">
+<div class="modal-content">
+<div class="modal-header">
+<button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">×</span><span class="sr-only">Close</span></button>
+<h4 class="modal-title" id="myModalLabel2">Donors Perks</h4>
+</div>
+<div class="modal-body">
+
+If you donate via gofundme please PM Vinnie as donations are now handled manually :)
+<br>
+Donation Options as follows:
+<br>
+<br>
+For VIP RANK
+<br>
+Become a V.I.P for 1 days PER donated USD if you donate 10 USD or less 
+<br>
+Become a V.I.P for 2 days PER donated USD if you donate between 11 USD and 20 USD 
+<br>
+Become a V.I.P for 3 days PER donated USD if you donate 21 USD or more 
+<br>
+EXAMPLE: $30 would get you 90 days VIP
+<br>
+<br>
+<p class="text-danger">OR</p>
+<br>
+For UPLOAD CREDIT
+<br>
+Get 5 GB PER donated USD if you donate 19 USD or less 
+<br>
+Get 8 GB PER donated USD if you donate between 20 USD and 29 USD 
+<br>
+Get 20 GB PER donated USD if you donate 30 USD or more
+<br>
+<br>
+<p class="text-danger">OR</p>
+<br>
+For FL SLOTS
+<br>
+Get 1 FreeLeech Slot PER USD donated
+<br>
+<br>
+<p class="text-danger">OR</p>
+<br>
+For INVITES
+<br>
+Get 1 Invite PER 5 USD donated
+<br>
+<br>
+VIP Rank includes the following perks:
+<br>
+Warnings removed
+1 Invite if donation is over 10 USD
+Hit and run log count cleared
+Immune to H&R System
+Immune to Low Ratio System
+Immune to Auto Warn and Ban System
+Higher Requests Priority 
+Donor Star
+Sparkle Effects Username
+Sitewide Freeleech
+VIP Forum Access (Includes Forum for other tracker invites)
+That warm fuzzy feeling that your helping the site O:-
+
+</div>
+<div class="modal-footer">
+<button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+</div>
+</div>
+</div>
+</div>';
 
 echo "<br /></td>\n</tr>\n</table>\n";
 
