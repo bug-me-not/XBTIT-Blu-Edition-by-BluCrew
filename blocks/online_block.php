52ec2fa71c0c700df4b2edb993b828e44c4f7d0d
HDVinnie <Vinnie981@gmail.com>
2016-06-25 19:43:23 -0400

    Online Block Update
    Unique Visitors

--- blocks/online_block.php
+++ blocks/online_block.php
@@ -114,14 +114,14 @@ else
      print("<tr><td colspan=\"2\" class=\"blocklist\">".$language["ACTIVATED"].": ".implode(", ",$uo)."</td>\n</tr>\n");
 
 {
-print("<table><tr><td align=\"center\">&nbsp;<img src=\"images/whos_online.gif\"><b>&nbsp;  (Total Users Online Now: <font color ='red'>".$total_online."</font>)</b></td>");
+print("<br><table><tr><td align=\"left\">&nbsp;<img src=\"images/whos_online.gif\"><b>&nbsp;  (Total Users Online Now: <font color ='red'>".$total_online."</font>)</b></td>");
+print("<td align=\"center\">&nbsp;<img src=\"images/whos_online.gif\"><b>&nbsp;  (Unique Visit Today: <font color ='red'>Coming Soon</font>)</b></td></tr></table>");
 }
 
      block_end();
 } // end if user can view
 
 ?>
-</table>
 <div class="panel-footer">
 </div>
 </div>
