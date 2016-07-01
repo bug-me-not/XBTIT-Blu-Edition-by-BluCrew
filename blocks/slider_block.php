3f8dd4c1460add16a7f5acd446fdc39b36b0baaf
HDVinnie <Vinnie981@gmail.com>
2016-07-01 16:23:50 -0400

    New Slider Block Testing

--- blocks/slider_block.php
+++ blocks/slider_block.php
@@ -7,7 +7,7 @@
     <body>
 <div class="panel panel-primary">
 <div class="panel-heading">
-<h4 class="text-center">Latest Internals(Under Development)</h4>
+<h4 class="text-center">Featured Torrent(Under Development)</h4>
 </div>
         <div class="container-fluid" style="padding:0;">
             <div class="row-fluid">
@@ -79,7 +79,7 @@
 
                     <!-- TORRENT INFO -->
                     <div>
-                    <h2>TITLE</h2>
+                    <h2>Tron Legacy 2010 Blu ray 1080p AVC DTS HD MA 7 1 tater44 BluRG</h2>
                     <p>Size: 10.05 GB | Seeders: 3 | Leechers: 0 | Completed: 25</p>
                     </div>
                     <!-- TORRENT INFO -->
