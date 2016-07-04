584b692fe3f98c7a28f6972f93402a49ffa3eae3
HDVinnie <Vinnie981@gmail.com>
2016-07-03 21:57:11 -0400

    slider updated

--- blocks/slider_block.php
+++ blocks/slider_block.php
@@ -1,3 +1,55 @@
+<?php
+global $BASEURL, $STYLEPATH, $dblist, $XBTT_USE,$btit_settings;
+if ($XBTT_USE)
+{
+   $tseeds="f.seeds+ifnull(x.seeders,0) as seeds";
+   $tleechs="f.leechers+ifnull(x.leechers,0) as leechers";
+   $tcompletes="f.finished+ifnull(x.completed,0) as finished";
+   $ttables="{$TABLE_PREFIX}files f LEFT JOIN xbt_files x ON x.info_hash=f.bin_hash";
+}
+else
+{
+   $tseeds="f.seeds as seeds";
+   $tleechs="f.leechers as leechers";
+   $tcompletes="f.finished as finished";
+   $ttables="{$TABLE_PREFIX}files f";
+}
+$exclude="";
+if ($XBTT_USE)
+$rowcat = do_sqlquery("SELECT u.id, (u.downloaded+IFNULL(x.downloaded,0)) as downloaded, ((u.uploaded+IFNULL(x.uploaded,0))/(u.downloaded+IFNULL(x.downloaded,0))) as uratio, cp.* FROM {$TABLE_PREFIX}users u LEFT JOIN xbt_users x ON x.uid=u.id INNER JOIN {$TABLE_PREFIX}categories_perm cp ON u.id_level=cp.levelid WHERE u.id = ".$CURUSER["uid"].";",true);
+else
+$rowcat = do_sqlquery("SELECT u.id, u.downloaded, (u.uploaded/u.downloaded) as uratio, cp.* FROM {$TABLE_PREFIX}users u INNER JOIN {$TABLE_PREFIX}categories_perm cp ON u.id_level=cp.levelid WHERE u.id = ".$CURUSER["uid"].";",true);
+if (sql_num_rows($rowcat)>0)
+while ($catdata=$rowcat->fetch_array())
+if($catdata["viewtorrlist"]!="yes" && (($catdata["downloaded"]>=$GLOBALS["download_ratio"] && ($catdata["ratio"]>$catdata["uratio"]))||($catdata["downloaded"]<$GLOBALS["download_ratio"])||($catdata["ratio"]=="0")))
+$exclude.=' AND f.category!='.$catdata['catid'];
+$sql = do_sqlquery("SELECT torrent_id FROM {$TABLE_PREFIX}featured ORDER BY fid DESC limit 1");
+$result = $sql->fetch_assoc();
+$torrent = do_sqlquery("SELECT f.moder,f.imdb,f.image, f.info_hash, f.filename, f.url, UNIX_TIMESTAMP(f.data) as data, f.size, f.comment, f.uploader, c.name as cat_name, c.image as cat_image, c.id as cat_id, $tseeds, $tleechs, $tcompletes, f.speed, f.external, f.announce_url,UNIX_TIMESTAMP(f.lastupdate) as lastupdate,UNIX_TIMESTAMP(f.lastsuccess) as lastsuccess, f.anonymous, u.username FROM $ttables LEFT JOIN {$TABLE_PREFIX}categories c ON c.id=f.category LEFT JOIN {$TABLE_PREFIX}users u ON u.id=f.uploader WHERE f.info_hash ='$result[torrent_id]' AND (f.team = 0 OR ".$CURUSER['id_level']."> 7) $exclude AND f.moder = 'ok'",true);
+$tor = $torrent->fetch_assoc();
+// progress
+$id = $tor['info_hash'];
+$subres = get_result("SELECT sum(IFNULL(bytes,0)) as to_go, count(*) as numpeers FROM {$TABLE_PREFIX}peers where infohash='{$id}'",true,$btit_settings['cache_duration']);
+$subres2 = get_result("SELECT size FROM {$TABLE_PREFIX}files WHERE info_hash ='{$id}'",true,$btit_settings['cache_duration']);
+$torrent = $subres2[0];
+$subrow = $subres[0];
+$tmp=0+$subrow["numpeers"];
+if ($tmp>0) {
+   $tsize=(0+$torrent["size"])*$tmp;
+   $tbyte=0+$subrow["to_go"];
+   $prgs=(($tsize-$tbyte)/$tsize) * 100; //100 * (1-($tbyte/$tsize));
+   $prgsf=floor($prgs);
+}
+else
+$prgsf=0;
+// progress end
+// speed
+if ($tor["speed"] <= 1)
+$speed = 0;
+else
+$speed = $tor["speed"]/1048576 ;
+// speed end
+?>
 
 <html>
     <head>
@@ -79,14 +131,14 @@
 
                     <!-- TORRENT INFO -->
                     <div>
-                    <h2>Tron Legacy 2010 Blu ray 1080p AVC DTS HD MA 7 1 tater44 BluRG</h2>
+                    <h2><a href="index.php?page=torrent-details&amp;id=<?php echo $tor['info_hash']; ?>"><?php echo $tor['filename']; ?></font></a></h2>
                     <p>Size: 10.05 GB | Seeders: 3 | Leechers: 0 | Completed: 25</p>
                     </div>
                     <!-- TORRENT INFO -->
 
                     <!-- DOWNLOAD BUTTON -->
                     <button class="btn btn-labeled btn-danger" type="button">
-                    <span class="btn-label"><i class="fa fa-download"></i></span><a href="#" target="_blank">Download</a></button>
+                    <span class="btn-label"><i class="fa fa-download"></i></span><a href="index.php?page=torrent-details&id=<?php echo $tor['info_hash']; ?>" alt="Torrent Details">Download</a></button>
                     <!-- DOWNLAOD BUTTON -->
 
                     <!-- NEXT-PREV NAVIGATION -->
