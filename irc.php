f6e4611eb3f737c7c31634b741516fd83eb455be
HDVinnie <Vinnie981@gmail.com>
2016-07-20 17:22:42 -0400

    Web IRC iframe added

--- irc.php
+++ irc.php
@@ -20,6 +20,18 @@ $text =
            </p>
            <div class=\"panel-footer\">
            </div>
+            </div>
+        </div>
+        </div>
+
+         <div class=\"row\">
+         <div class=\"col-md-12\">
+         <div class=\"panel panel-primary\">
+         <div class=\"panel-heading\">
+         <h2 class=\"text-center\"></h2></div>
+         <center><iframe width=\"550\" height=\"350\" scrolling=\"no\" src=\"http://widget.mibbit.com/?settings=c9cfb89f40f649efc2ffe7c60f80a20c&server=irc.blurg.xyz&channel=%23blurg\"></iframe></center>
+         <div class=\"panel-footer\">
+        </div>
         </div>
         </div>
         </div>
@@ -31,3 +43,4 @@ $irctpl->set("irc_text",$text);
 ob_end_flush();
 
 ?>
+
