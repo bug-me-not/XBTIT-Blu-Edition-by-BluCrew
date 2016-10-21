62354563907e92597c2c8bbf03cbb77b983e7309
HDVinnie <Vinnie981@gmail.com>
2016-10-21 12:00:34 -0400

    Recommended Clients Block

---
+++ blocks/clients_block.php
@@ -0,0 +1,59 @@
+<div class="panel panel-primary">
+<div class="panel-heading">
+<h4 class="text-center">Recommended BitTorrent Clients</h4>
+</div>
+<table class='table table-bordered' align='center' cellspacing='0' cellpadding='10' width='100%' border='1'><tbody>
+<tr align="center">
+<td align="center"><div class='index-torrent-c'>The following clients have been tested and approved for use at Sitename. </div><br>
+	<div id='index-btclients'>
+	<div id='index-clients-client'>
+	<div id='index-clients-graphic'>
+	<img src='/images/btclients/utorrent.png'>
+	</div>
+	<h15><a href='http://www.oldapps.com/utorrent.php?old_utorrent=45'>uTorrent 3.0</a></h15>&nbsp;&nbsp;(Windows)
+	</div>
+	<div id='index-clients-client'>
+	<div id='index-clients-graphic'>
+	<img src='/images/btclients/bittorrent.png'>
+	</div>
+	<h15><a href='http://www.oldapps.com/bittorrent.php?old_bittorrent=6633'>BiTorrent 7.5</a></h15>&nbsp;&nbsp;(Windows)
+	</div>
+	<div id='index-clients-client'>
+	<div id='index-clients-graphic'>
+	<img src='/images/btclients/rtorrent.png'>
+	</div>
+	<h15><a href='http://mac.softpedia.com/get/Internet-Utilities/RTorrent.shtml'>rTorrent 0.9.2/3</a></h15>&nbsp;&nbsp;(Mac/Linux)
+	</div>
+	<div id='index-clients-client'>
+	<div id='index-clients-graphic'>
+	<img src='/images/btclients/transmission.png'>
+	</div>
+	<h15><a href='http://www.oldapps.com/mac/transmission.php?old_transmission=9334'>Tranmission 2.03</a></h15>&nbsp;(Mac/Linux)
+	</div>
+	<div id='index-clients-client'>
+	<div id='index-clients-graphic'>
+	<img src='/images/btclients/deluge.png'>
+	</div>
+	<h15><a href='http://deluge-torrent.org/'>Deluge 1.3</a></h15>&nbsp;&nbsp;(Win/Mac/Linux)
+	</div>
+  <div id='index-clients-client'>
+	<div id='index-clients-graphic'>
+	<img src='/images/btclients/qbit.png'>
+	</div>
+	<h15><a href='http://sourceforge.net/projects/qbittorrent/files/qbittorrent-win32/qbittorrent-2.8.5//'>qBittorrent 2.8.5</a></h15>&nbsp;&nbsp;(Win/Mac)
+	</div>
+ 	<br style='clear: left;'>
+	</div>
+<br>
+<div class='index-torrent-c'>No other BitTorrent clients are supported, if you decide to use a non-listed client we cannot offer support with HnR or ratio problems.
+This list will be updated on a regular basis.</div><br>
+<div class='index-torrent-b'>The following BitTorrent clients have been proven to report incorrect data to the tracker, and usage may result in a ban: aria2, aria 1.9.0, BitLet 0.1, NSPlayer 9.0.0.2000, SymTorrent, WMTR, wmTorrent 2.1.0.6, justseed.it, JS0900 client, KTorrent, Transmission 2.04 and above, Tixati, Cloud-based Servers,
+.
+</div><br>
+</td>
+</tr>
+</tbody>
+</table>
+<div class="panel-footer">
+</div>
+</div>
