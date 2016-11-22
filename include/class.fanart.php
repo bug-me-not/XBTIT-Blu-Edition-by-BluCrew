fe4fa9d48f77974e8c7b05086b82271235d37faa
MrG01 <MrG01@users.noreply.github.com>
2016-11-22 12:49:17 +0200

    Fanart Update
    - Some syntax changes
    - Layout edits
    - new CD Art function relating to movies

--- include/class.fanart.php
+++ include/class.fanart.php
@@ -13,54 +13,54 @@
 
 class fanart
 {
-   private $api_key="";
+   private $api_key = "";
    private $movies;
    private $tv;
    private $url_data;
    private $url_tv;
    public $id;
-   public $error='';
+   public $error = '';
    public $error_code;
 
-   function __construct($id,$api_key)
+   function __construct($id , $api_key)
    {
-      $this->id=$id;
-      $this->api_key=$api_key;
-      $this->url_movies="http://webservice.fanart.tv/v3/movies/{$this->id}";
-      $this->url_tv="http://webservice.fanart.tv/v3/tv/{$this->id}";
+      $this->id = $id;
+      $this->api_key = $api_key;
+      $this->url_movies = "http://webservice.fanart.tv/v3/movies/{$this->id}";
+      $this->url_tv = "http://webservice.fanart.tv/v3/tv/{$this->id}";
    }
 
-   public function fetch($tv=FALSE)
+   public function fetch($tv = FALSE)
    {
       $info_curl;
       $url;
 
       if($tv)
-      $url=$this->url_tv;
+      $url = $this->url_tv;
       else
-      $url=$this->url_movies;
+      $url = $this->url_movies;
 
-      $info_curl=curl_init();
+      $info_curl = curl_init();
       curl_setopt($info_curl,CURLOPT_RETURNTRANSFER,1);
       curl_setopt($info_curl,CURLOPT_HTTPHEADER, array("api-key: {$this->api_key}"));
       curl_setopt($info_curl,CURLOPT_URL,$url);
-      $temp=curl_exec($info_curl);
+      $temp = curl_exec($info_curl);
 
-      $this->error_code=curl_getinfo($info_curl,CURLINFO_HTTP_CODE);
+      $this->error_code = curl_getinfo($info_curl,CURLINFO_HTTP_CODE);
 
       curl_close($info_curl);
 
-      if($this->error_code!=200 || empty($temp))
+      if($this->error_code != 200 || empty($temp))
       {
-         $this->error='Unable to get information from TVDB';
+         $this->error = 'Unable to get information from TVDB';
          return FALSE;
       }
       else
       {
          if($tv)
-         $this->tv=$temp;
+         $this->tv = $temp;
          else
-         $this->movies=$temp;
+         $this->movies = $temp;
 
          return TRUE;
       }
@@ -78,27 +78,27 @@ class fanart
 
    public function getimdb()
    {
-      $temp=$this->decode_json($this->movies);
+      $temp = $this->decode_json($this->movies);
       return str_replace("tt","",$temp['imdb_id']);
    }
 
    public function gettvdb()
    {
-      $temp=$this->decode_json($this->tv);
+      $temp = $this->decode_json($this->tv);
       return $temp['thetvdb_id'];
    }
 
    public function getmoviebanner()
    {
-      $bann=$this->decode_json($this->movies)['moviebanner'];
-      $temp=array();
+      $bann = $this->decode_json($this->movies)['moviebanner'];
+      $temp = array();
 
-      $count=((count($bann)>2)?2:count($bann));
+      $count = ((count($bann)>2) ? 2 : count($bann));
 
-      for($i=0;$i<$count;$i++)
+      for($i = 0; $i < $count; $i++)
       {
-         if($bann[$i]['lang']=='en')
-         $temp[]=$bann[$i]['url'];
+         if($bann[$i]['lang'] == 'en')
+         $temp[] = $bann[$i]['url'];
       }
 
       return $temp;
@@ -106,17 +106,33 @@ class fanart
 
    public function gettvbanner()
    {
-      $bann=$this->decode_json($this->tv)['tvbanner'];
-      $temp=array();
+      $bann = $this->decode_json($this->tv)['tvbanner'];
+      $temp = array();
 
-      $count=((count($bann)>2)?2:count($bann));
+      $count = ((count($bann)>2) ? 2 : count($bann));
 
-      for($i=0;$i<$count;$i++)
+      for($i = 0; $i < $count; $i++)
       {
-         if($bann[$i]['lang']=='en')
-         $temp[]=$bann[$i]['url'];
+         if($bann[$i]['lang'] == 'en')
+         $temp[] = $bann[$i]['url'];
       }
 
       return $temp;
+   }
+
+   public function getcdart()
+   {
+       $cdart = $this->decode_json($this->movies)['moviedisc'];
+       $temp = array();
+
+       $count = ((count($cdart) > 4) ? 4 : count($cdart));
+
+       for($i = 0; $i < $count; $i++)
+       {
+           if($cdart['$i']['lang'] == 'en')
+           $temp[] = $cdart[$i]['url'];
+       }
+
+       return $temp;
    }
 }
